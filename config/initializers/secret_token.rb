4bbe47e8afe94695c0f7b1e64499f8e3c4b9a7b1
Maxwell White <maxehmumpowa@gmail.com>
2014-01-28 21:37:00 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NimbleDigital::Application.config.secret_key_base = '6920febf226391523b8a6a62ea490f1e4c451facce55b01aed7849dfd9945aeaf440a7bed1efc52b2e308c5d23b27ec59dbd2b307d3962359b88765beff25078'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NimbleDigital::Application.config.session_store :cookie_store, key: '_nimble_digital_session'
